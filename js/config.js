95ed89f182855b19e31c60a51e67301329475ac8
akhilrane1017 <akhilrane1017@gmail.com>
2025-05-04 15:45:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_zG5m56GhF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '57hi9bke77fna8pd82l0gg74sk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_ncuR9Kua2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '76b10un37hmcv14toopj9od51i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
